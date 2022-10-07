2ff711fc4928634acd30c12b03b48e5b7822fe97
Matt Kelly <bmatthewkelly@gmail.com>
2022-10-06 17:48:02 -0700

    fix home nav logo blocking menu

--- components/Nav.js
+++ components/Nav.js
@@ -20,9 +20,9 @@ export default function Nav() {
             <a>
               <KLogo
                 width="30"
-                colorRect="fill-primary-light"
-                colorTop="fill-secondary-dark"
-                colorBottom="fill-secondary-light"
+                colorRect="fill-secondary-light"
+                colorTop="fill-primary-light"
+                colorBottom="fill-secondary-dark"
               />
             </a>
           </Link>
