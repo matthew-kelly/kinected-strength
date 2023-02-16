a3d618147441ac1180409e856ec0c721dbbfeacc
Matt Kelly <bmatthewkelly@gmail.com>
2023-02-15 16:12:49 -0800

    deal with iOS/MacOS css animation bug

--- components/LogoSpinner.js
+++ components/LogoSpinner.js
@@ -1,23 +1,41 @@
+import { motion } from "framer-motion";
 import styles from "./LogoSpinner.module.css";
 
 export default function LogoSpinner({ size = 100, textClass = "" }) {
+  // HACK: iOS currently has an unresolved bug where transforms cannot be animated, using framer-motion instead of css
   return (
-    <svg height={size} viewBox="0 0 100 100">
-      <defs>
-        <path
-          id="circlepath"
-          d="
+    <motion.div
+      style={{
+        originX: "50%",
+        originY: "50%",
+        transformBox: "fill-box",
+      }}
+      animate={{
+        rotate: -360,
+      }}
+      transition={{
+        duration: 12,
+        repeat: Infinity,
+        ease: "linear",
+      }}
+    >
+      <svg height={size} viewBox="0 0 100 100">
+        <defs>
+          <path
+            id="circlepath"
+            d="
               M 25, 50
               a 25,25 0 1,1 50,0
               a 25,25 0 1,1 -50,0
             "
-        />
-      </defs>
-      <text
-        className={`${styles.spinner} uppercase font-display font-extrabold fill-primary-dark text-[9px] tracking-[0.125rem] select-none ${textClass}`}
-      >
-        <textPath xlinkHref="#circlepath">Kinected • Strength</textPath>
-      </text>
-    </svg>
+          />
+        </defs>
+        <text
+          className={`uppercase font-display font-extrabold fill-primary-dark text-[9px] tracking-[0.125rem] select-none ${textClass}`}
+        >
+          <textPath xlinkHref="#circlepath">Kinected • Strength</textPath>
+        </text>
+      </svg>
+    </motion.div>
   );
 }
