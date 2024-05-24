7c051a1cec92a235a9dc8297615f13a9f1b6eb80
Matt Kelly <bmatthewkelly@gmail.com>
2024-05-24 15:27:38 -0700

    limit site image sizes to max 1440px wide

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         pathname: `/images/${process.env.NEXT_PUBLIC_SANITY_API_PROJECT_ID}/**`,
       },
     ],
-    deviceSizes: [448, 512, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [448, 512, 640, 750, 828, 1080, 1200, 1440], //1600, 1920 2048, 3840
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 432],
   },
   i18n: {
