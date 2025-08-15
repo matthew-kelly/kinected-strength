6c420213ac4520194b841d9649c228e449bb4802
Matt Kelly <bmatthewkelly@gmail.com>
2025-08-15 16:23:42 -0700

    move to nodemailer for contact email, remove sendgrid

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,22 +1,33 @@
-import sendgrid from "@sendgrid/mail";
-
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+import nodemailer from "nodemailer";
 
 export default async function sendEmail(req, res) {
+  const { name, email, reason, message } = req.body;
+
   try {
-    const email = {
+    // Initializing the SMTP connection
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+    // Define email content
+    const mailOptions = {
+      from: process.env.FROM_EMAIL,
       to: process.env.ADMIN_EMAIL,
-      from: process.env.SENDGRID_EMAIL,
-      subject: `New email from ${req.body.name}`,
+      subject: `New email from ${name}`,
       html: `
       <div>
-        <p>Name: ${req.body.name}</p><br />
-        <p>Email: ${req.body.email}</p><br />
-        ${req.body?.reason && `<p>Reason: ${req.body.reason}</p><br />`}
-        <p>Message: ${req.body.message}</p>
+        <p>Name: ${name}</p><br />
+        <p>Email: ${email}</p><br />
+        ${reason && `<p>Reason: ${reason}</p><br />`}
+        <p>Message: ${message}</p>
       </div>`,
     };
-    await sendgrid.send(email);
+    // Function to send the email
+    await transporter.sendMail(mailOptions);
   } catch (error) {
     return res.status(error.statusCode || 500).json({ error: error.message });
   }
